3acb50b5498ef89d00cd85bb98f533c04de042b1
Hasan GÖKALP <Hasan GKALP@DESKTOP-R04PVQ3>
2024-01-30 16:32:29 +0300

    Changes and adding options for DI

--- Prism.Module.First/MainScreen.vb
+++ Prism.Module.First/MainScreen.vb
@@ -1,19 +1,25 @@
-﻿Imports System.Reflection
-Imports Prism.Ioc
+﻿Imports Prism.Ioc
 Imports Prism.Modularity
 Imports Prism.Regions
 
 Public Class MainScreen
     Implements IModule
 
+    Private ReadOnly _regionmanager As IRegionManager
+
+    Public Sub New(regionmanager As IRegionManager)
+        _regionmanager = regionmanager
+    End Sub
+
     Public Sub RegisterTypes(containerRegistry As IContainerRegistry) Implements IModule.RegisterTypes
         containerRegistry.Register(Of ViewMain)
+        'containerRegistry.RegisterSingleton(Of IRegionManager, RegionManager)()
     End Sub
 
     Public Sub OnInitialized(containerProvider As IContainerProvider) Implements IModule.OnInitialized
-        '_regionManager.RegisterViewWithRegion("Screen", GetType(ModuleC))
-        Dim region = containerProvider.Resolve(Of IRegionManager)()
-        region.RegisterViewWithRegion("MainScreen", GetType(ViewMain))
+        _regionmanager.RegisterViewWithRegion("MainScreen", GetType(ViewMain))
+        'Dim region = containerProvider.Resolve(Of IRegionManager)()
+        'region.RegisterViewWithRegion("MainScreen", GetType(ViewMain))
     End Sub
 
 End Class
